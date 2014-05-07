02e5d85fd5d13edfdc54f0ad49040695b7753e6e
Nick Baugh <niftylettuce@gmail.com>
2014-05-07 03:32:26 -0400

    Added pip to deps

--- install-deps.sh
+++ install-deps.sh
@@ -4,5 +4,5 @@ set -e -x
 VER=$TRAVIS_PYTHON_VERSION
 
 apt-get update -yqq || true
-apt-get install -yqq python$VER
+apt-get install -yqq python$VER python-pip
 pip install -q requests protobuf cherrypy ws4py python-mpd2 lxml web.py --upgrade
